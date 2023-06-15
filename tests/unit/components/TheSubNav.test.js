49d05e8c679b9811622e1307dec129067e8b54ca
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-06-15 23:38:12 +0300

    added subnav, tests for it, fontawesome, stub it

---
+++ tests/unit/components/TheSubNav.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/vue";
+import TheSubNav from "@/components/TheSubNav.vue";
+
+describe("TheSubNav", () => {
+  describe("when the user on the jobs page", () => {
+    it("displays job count", () => {
+      render(TheSubNav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultsPage: true,
+          };
+        },
+      });
+
+      const jobCount = screen.getByText("1653");
+      expect(jobCount).toBeInTheDocument();
+    });
+  });
+
+  describe("when the user on the jobs page", () => {
+    it("does not display job count", () => {
+      render(TheSubNav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultsPage: false,
+          };
+        },
+      });
+      const jobCount = screen.queryByText("1653");
+      expect(jobCount).not.toBeInTheDocument();
+    });
+  });
+});
