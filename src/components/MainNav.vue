2505fa8e323d5f4c0670ad9f9bf87b3daec664ae
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-06-15 09:46:00 +0300

    added menuItems, added test for it

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,9 +2,18 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div
-        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto"
-      >
+        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
         <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="item in menuItems"
+              :key="item"
+              class="ml-9 h-full first:ml-0">
+              <a class="flex h-full items-center py-2.5" href="/">{{ item }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +25,14 @@ export default {
   data() {
     return {
       company: "Google",
+      menuItems: [
+        "Teams",
+        "Location",
+        "Life at Google",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,18 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Google");
     expect(companyName).toBeInTheDocument();
   });
+
+  it("displays menu items", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navMenuText = navigationMenuItems.map((item) => item.textContent);
+    expect(navMenuText).toEqual([
+      "Teams",
+      "Location",
+      "Life at Google",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
