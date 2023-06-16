34054abbb673fa84c064561a78172e2b25599da7
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-06-16 23:42:20 +0300

    added theheadline component and interval logic for changing action text

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <main-nav />
+  <the-hero />
+  <div>11</div>
 </template>
 
 <script>
 import MainNav from "@/components/MainNav.vue";
+import TheHero from "@/components/TheHero.vue";
 
 export default {
   name: "App",
   components: {
     MainNav,
+    TheHero,
   },
 };
 </script>
