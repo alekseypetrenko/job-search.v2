d3366d2e1d6cd94b9b8f7c800b8ca4d3aff99dcf
oleksii.petrenko <oleksii.petrenko@valtech.com>
2023-07-11 01:00:34 +0300

    added sidebar and job listings components

---
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex flex-col border-r border-solid border-brand-gray-1 bg-white p-4 w-96">
+    JobFilterSidebar
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobFilterSidebar",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/JobResults/JobListings.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex-auto bg-brand-gray-2 p-8">Job Listing</main>
+</template>
+
+<script>
+export default {
+  name: "JobListing",
+};
+</script>
+
+<style lang="scss" scoped></style>
