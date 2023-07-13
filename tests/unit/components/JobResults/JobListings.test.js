b73e1ddd4fa29176024361a01c94538efac42132
oleksii.petrenko <oleksii.petrenko@valtech.com>
2023-07-13 23:34:43 +0300

    display only 10 jobs per page, fixed test

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -7,27 +7,36 @@ vi.mock("axios");
 describe("JobListings", () => {
   it("fetches jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
+    const $route = { query: { page: "5" } };
     render(JobListings, {
       global: {
         stubs: {
           "router-link": RouterLinkStub,
+        },
+        mocks: {
+          $route,
         },
       },
     });
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
-  it("it creates a job listings for every job", async () => {
+  it("it displayes max of 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const $route = { query: { page: "1" } };
+
     render(JobListings, {
       global: {
         stubs: {
           "router-link": RouterLinkStub,
         },
+        mocks: {
+          $route,
+        },
       },
     });
 
     const jobListings = await screen.findAllByRole("listitem");
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
   });
 });
