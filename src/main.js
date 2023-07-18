4c085efeb2955bba7216058fca7c62f03b63be5e
oleksii.petrenko <oleksii.petrenko@valtech.com>
2023-07-19 01:49:38 +0300

    added and registered pinia

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import router from "./router";
+import { createPinia } from "pinia";
 
 import "@/index.css";
 import App from "@/App.vue";
 
 library.add(faSearch);
 
+const pinia = createPinia();
+
 createApp(App)
+  .use(pinia)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
