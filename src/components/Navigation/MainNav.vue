9ca4d4c97e7b4b042ab9fe24a81360ab304a5a23
oleksii.petrenko <oleksii.petrenko@valtech.com>
2023-07-19 22:59:14 +0300

    added mapActions and mapState

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -22,18 +22,18 @@
         </nav>
 
         <div class="ml-auto flex h-full items-center">
-          <profile-image v-if="userStore.isLoggedIn" />
-          <action-button v-else text="Sign In" @click="userStore.loginUser" />
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else text="Sign In" @click="loginUser" />
         </div>
       </div>
 
-      <the-sub-nav v-if="userStore.isLoggedIn" />
+      <the-sub-nav v-if="isLoggedIn" />
     </div>
   </header>
 </template>
 
 <script>
-import { mapStores } from "pinia";
+import { mapActions, mapState } from "pinia";
 import { useUserStore } from "@/stores/user";
 
 import ActionButton from "@/components/Shared/ActionButton.vue";
@@ -60,13 +60,16 @@ export default {
     };
   },
   computed: {
-    ...mapStores(useUserStore),
+    ...mapState(useUserStore, ["isLoggedIn"]),
     headerHeightClass() {
       return {
-        "h-16": !this.userStore.isLoggedIn,
-        "h-32": this.userStore.isLoggedIn,
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn,
       };
     },
+  },
+  methods: {
+    ...mapActions(useUserStore, ["loginUser"]),
   },
 };
 </script>
