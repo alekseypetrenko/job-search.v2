ffa4973837d2328d24cc73a26104377e9b903911
oleksii.petrenko <oleksii.petrenko@valtech.com>
2023-07-25 15:34:14 +0300

    added Accordion component, font-awesome icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faAngleDown,
+  faAngleUp,
+} from "@fortawesome/free-solid-svg-icons";
 import router from "./router";
 import { createPinia } from "pinia";
 
@@ -9,6 +13,8 @@ import "@/index.css";
 import App from "@/App.vue";
 
 library.add(faSearch);
+library.add(faAngleDown);
+library.add(faAngleUp);
 
 const pinia = createPinia();
 
