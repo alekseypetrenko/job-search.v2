406c830e455f737fc2b1c03c7a69a3630e470621
oleksii.petrenko <oleksii.petrenko@valtech.com>
2023-07-25 22:41:52 +0300

    moved Organizations to the separete component

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -9,69 +9,20 @@
         </div>
       </div>
 
-      <collapsible-accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="h-8 w-1/2">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Between Vue" type="checkbox" class="mr-3" />
-                <label for="Between Vue">Between Vue</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="At Vue Brute" type="checkbox" class="mr-3" />
-                <label for="At Vue Brute">At Vue Brute</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Vue and Half Man" type="checkbox" class="mr-3" />
-                <label for="Vue and Half Man">Vue and Half Man</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
-      <collapsible-accordion header="Degrees">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="h-8 w-1/2">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Between Vue" type="checkbox" class="mr-3" />
-                <label for="Between Vue">Between Vue</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="At Vue Brute" type="checkbox" class="mr-3" />
-                <label for="At Vue Brute">At Vue Brute</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Vue and Half Man" type="checkbox" class="mr-3" />
-                <label for="Vue and Half Man">Vue and Half Man</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
+      <job-filter-sidebar-organizations />
     </section>
   </div>
 </template>
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
-import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+import JobFilterSidebarOrganizations from "./JobFilterSidebarOrganizations.vue";
 
 export default {
   name: "JobFilterSidebar",
   components: {
     ActionButton,
-    CollapsibleAccordion,
+    JobFilterSidebarOrganizations,
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
---
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue
@@ -0,0 +1,37 @@
+<template>
+  <collapsible-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="Between Vue" type="checkbox" class="mr-3" />
+            <label for="Between Vue">Between Vue</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="At Vue Brute" type="checkbox" class="mr-3" />
+            <label for="At Vue Brute">At Vue Brute</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="Vue and Half Man" type="checkbox" class="mr-3" />
+            <label for="Vue and Half Man">Vue and Half Man</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script>
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
+export default {
+  name: "JobFilterSidebarOrganizations",
+  components: {
+    CollapsibleAccordion,
+  },
+};
+</script>
